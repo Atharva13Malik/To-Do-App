4be36b95589a0beaca9e363a200b5c443fb91c96
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-02-12 09:50:47 +0530

    3 columns of ToDo app

--- ToDo-app-project/src/App.jsx
+++ ToDo-app-project/src/App.jsx
@@ -9,9 +9,42 @@ function App() {
       <h1>TODO APP</h1>
 
       <div className="row">
-        <div className="col-6">col-4</div>
-        <div className="col-4">col-4</div>
-        <div className="col-2">col-4</div>
+        <div className="col-6">
+          <input type="text" placeholder="Enter Todo Here" />
+        </div>
+        <div className="col-4">
+          <input type="text" placeholder="Enter Todo Here" />
+        </div>
+        <div className="col-2">
+          <button className="btn btn-success ms-5">Add</button>
+
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+         Buy Milk
+        </div>
+        <div className="col-4">
+         4/10/2023
+        </div>
+        <div className="col-2">
+         <button type="button" class="btn btn-outline-danger">Delete</button>
+
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <input type="text" placeholder="Enter Todo Here" />
+        </div>
+        <div className="col-4">
+          <input type="text" placeholder="Enter Todo Here" />
+        </div>
+        <div className="col-2">
+          <button className="btn btn-success ms-5">Add</button>
+
+        </div>
       </div>
     </div>
   );
